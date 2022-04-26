2e7bfd9e5e0bef0409b474b818aeee7946374a47
Võ Minh Khoa <vmkkhoa@gmail.com>
2022-04-26 15:23:19 +0700

    small update

--- AI.py
+++ AI.py
@@ -3,14 +3,14 @@ from PIL import Image, ImageOps
 import numpy as np
 
 # Load the model
-model = load_model('keras_model.h5')
+model = load_model('keras_model_example.h5')
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 # Replace this with the path to your image
-image = Image.open('<IMAGE_PATH>')
+image = Image.open('test.jpg')
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
